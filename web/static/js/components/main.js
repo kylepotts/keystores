e937d63e2b403ffb6c38e4639a098c168f2ebb27
Kyle Potts <pottsk@purdue.edu>
2016-05-01 22:19:05 -0400

    basic intro page started

--- web/static/js/components/main.js
+++ web/static/js/components/main.js
@@ -1,12 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import {Navbar, Jumbotron, Button} from 'react-bootstrap'
+
+import "../../css/app.css";
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello Dude</h1>
-        <a href="/about">here</a>
+        <Navbar fixedTop={true}>
+          <Navbar.Brand>
+            <a href="#">Keystores</a>
+          </Navbar.Brand>
+        </Navbar>
+        <div>
+          <Jumbotron id='jumbo-parent'>
+            <div id='jumbo-child'>
+              <h1>Keystores</h1>
+              <p>Tired of trying to sync your environment vairbales, use KeyStores to sync for them you!</p>
+              <p><Button bsStyle='primary'>Sign Up</Button></p>
+            </div>
+          </Jumbotron>
+        </div>
       </div>
     )
   }
